0d19f6e3e0546f9d682bffe1beb4357d668d6316
elizabethvilloria <lizvilloria@gmail.com>
2024-03-03 22:22:44 +0100

    wip

--- api/projects/model.js
+++ api/projects/model.js
@@ -1,6 +1,24 @@
 // build your `Project` model here
-function getProjectById(project_id) {
-    return Promise.resolve(`PROJECT ${project_id}`)
+const db = require('../../data/dbConfig')
+
+async function getAllProjects() {
+    return db('projects');
 }
 
-module.exports = { getProjectById }
+async function getProjectById(project_id) {
+    const projectRows = await db('projects as p')
+        .where('project_id', project_id);
+    
+    return projectRows[0] || null;
+}
+
+async function addProject(project) {
+    const [project_id] = await db('projects').insert(project, 'project_id');
+    return db('projects').where({ project_id }).first();
+}
+
+module.exports = {
+    getAllProjects,
+    getProjectById,
+    addProject,
+};
--- api/projects/router.js
+++ api/projects/router.js
@@ -2,13 +2,37 @@
 const router = require('express').Router()
 const projectModel = require('./model.js')
 
-router.get('/:project_id', (req, res, next) => {
-    projectModel.getProjectById(req.params.project_id)
-        .then(resource => {
-            res.status(200).json(resource)
-        })
-        .catch(next)
-})
+// GET /api/projects/:project_id
+router.get('/', async (req, res, next) => {
+    try {
+      const projects = await projectModel.getAllProjects();
+      res.status(200).json(projects);
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+
+// POST /api/projects
+router.post('/', async (req, res, next) => {
+    try {
+        const { project_name, project_description, project_completed } = req.body;
+
+        if (!project_name) {
+            return res.status(400).json({ error: 'project_name is required' });
+        }
+
+        const newProject = await projectModel.addProject({
+            project_name,
+            project_description,
+            project_completed: Boolean(project_completed) || false,
+        });
+
+        res.status(201).json(newProject);
+    } catch (error) {
+        next(error);
+    }
+});
 
 router.use((err, req, res, next) => { // eslint-disable-line
     res.status(500).json({
