f331e02b55bdf2cb81b8cef7c6171121b5879c36
elizabethvilloria <lizvilloria@gmail.com>
2024-03-03 20:38:02 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here and require it from index.js
+const express = require('express')
+const projectRouter = require('./projects/router.js')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/projects', projectRouter)
+
+server.use('*', (req, res) => {
+    res.json({ api: 'up' })
+})
+
+module.exports = server;
