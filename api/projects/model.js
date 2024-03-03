4e9aacd5d8df74d8300fbe7a8b559d6fc3c7a0da
elizabethvilloria <lizvilloria@gmail.com>
2024-03-03 20:59:41 +0100

    wip

--- api/projects/model.js
+++ api/projects/model.js
@@ -1,1 +1,6 @@
 // build your `Project` model here
+function getProjectById(project_id) {
+    return Promise.resolve(`PROJECT ${project_id}`)
+}
+
+module.exports = { getProjectById }
--- api/projects/router.js
+++ api/projects/router.js
@@ -1,1 +1,21 @@
 // build your `/api/projects` router here
+const router = require('express').Router()
+const projectModel = require('./model.js')
+
+router.get('/:project_id', (req, res, next) => {
+    projectModel.getProjectById(req.params.project_id)
+        .then(resource => {
+            res.status(200).json(resource)
+        })
+        .catch(next)
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        customMessage: 'ERROR in projects router',
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = router
